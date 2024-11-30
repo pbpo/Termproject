6088e1868b043bc9e7d3db5f044bc5f55c891475
안정후(AHN JEONGHU) <afterrest00@gmail.com>
2024-12-01 00:12:34 +0900

    atm 여러개

--- Termproject/AdminMenuTest.cpp
+++ Termproject/AdminMenuTest.cpp
@@ -73,10 +73,11 @@ void AdminMenuTest::initializeSystemWithTestData() {
     SecurityManager::getInstance()->addUser("4", "4", "우리");
 
     // Add some cash to the ATM
-    CashManager::getInstance()->addCash(Denomination::KRW_1000, 50);
-    CashManager::getInstance()->addCash(Denomination::KRW_5000, 40);
-    CashManager::getInstance()->addCash(Denomination::KRW_10000, 30);
-    CashManager::getInstance()->addCash(Denomination::KRW_50000, 20);
+    CashManager* cashManager = new CashManager();
+    cashManager->addCash(Denomination::KRW_1000, 50);
+    cashManager->addCash(Denomination::KRW_5000, 40);
+    cashManager->addCash(Denomination::KRW_10000, 30);
+    cashManager->addCash(Denomination::KRW_50000, 20);
 
     // Choose one bank as primary (e.g., 카카오)
     std::shared_ptr<Bank> primaryBank(kakaoBank, [](Bank*) {
@@ -85,7 +86,7 @@ void AdminMenuTest::initializeSystemWithTestData() {
 
 
     // Create and initialize the ATM
-    ATM atm("123456", ATMType::MULTI, manager, true); // Multi-bank ATM
+    ATM atm("123456", ATMType::MULTI, cashManager, true); // Multi-bank ATM
 
     // Set ATM and primary bank in SystemStatus
     SystemStatus::getInstance()->setATM(&atm);
