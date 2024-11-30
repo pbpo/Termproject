ca2f29792cb7d00e93d2111bd638127e14ad9745
안정후(AHN JEONGHU) <afterrest00@gmail.com>
2024-12-01 00:06:00 +0900

    atm 여러개

--- Termproject/TransferTransaction.hpp
+++ Termproject/TransferTransaction.hpp
@@ -5,29 +5,29 @@
 //  Created by cho on 10/20/24.
 //
 
-    #pragma once
+#pragma once
 
-    #include "ITransaction.hpp"
-    #include "Account.hpp"
-    #include "Utility.hpp"
-    #include <memory>
+#include "ITransaction.hpp"
+#include "Account.hpp"
+#include "Utility.hpp"
+#include <memory>
 
-    class TransferTransaction : public ITransaction {
-    private:
-        std::shared_ptr<Account> sourceAccount;
-        std::shared_ptr<Account> destinationAccount;
-        TransferType transferType;
-        int fee;
+class TransferTransaction : public ITransaction {
+private:
+    std::shared_ptr<Account> sourceAccount;
+    std::shared_ptr<Account> destinationAccount;
+    TransferType transferType;
+    int fee;
 
-    public:
-        TransferTransaction(const std::string& transactionID, int amount, const std::shared_ptr<Account>& sourceAccount, const std::shared_ptr<Account>& destinationAccount, TransferType transferType, const std::string& cardNumber);
+public:
+    TransferTransaction(CashManager* cashManager, const std::string& transactionID, int amount, const std::shared_ptr<Account>& sourceAccount, const std::shared_ptr<Account>& destinationAccount, TransferType transferType, const std::string& cardNumber);
 
-        bool execute() override;
-        void rollback() override;
-        void printDetails() const override;
-        std::string getTransactionType() const override {
-            return "Transfer";
-        }
+    bool execute() override;
+    void rollback() override;
+    void printDetails() const override;
+    std::string getTransactionType() const override {
+        return "Transfer";
+    }
 
-        int getFee() const { return fee; }
-    };
+    int getFee() const { return fee; }
+};
